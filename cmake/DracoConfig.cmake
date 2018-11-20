50bd27c87fa09c4721c66b51c828b67d5d2a41c1
Thomas Blocher <thomas.blocher@viimle.com>
2018-11-20 11:51:33 +0100

    Improved CMake install script

--- cmake/DracoConfig.cmake
+++ cmake/DracoConfig.cmake
@@ -1,3 +1,1 @@
-@PACKAGE_INIT@
-set_and_check(draco_INCLUDE_DIR "@PACKAGE_draco_include_install_dir@")
-set_and_check(draco_LIBRARY_DIR "@PACKAGE_draco_lib_install_dir@")
+include("${CMAKE_CURRENT_LIST_DIR}/DracoTargets.cmake")
